3a8368423057bee02c79c0d1c10de8f297c55813
Thierry Pelais de Oliveira <compasso-thierry.oliveira@ailos.coop.br>
2022-07-06 00:59:10 -0300

    feat: add method handler to call right method to get battery level on android

--- android/app/src/main/kotlin/com/example/flutter_platform_specific_examples/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_platform_specific_examples/MainActivity.kt
@@ -11,7 +11,26 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "samples.thierryoliveira.com/battery"
 
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+                call, result ->
+            if (call.method == "getBatteryLevel") {
+                val batteryLevel = getBatteryLevel()
+
+                if (batteryLevel != -1) {
+                    result.success(batteryLevel)
+                } else {
+                    result.error("UNAVAILABLE", "Baattery level not available", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+
+    }
 
     private fun getBatteryLevel(): Int {
         val batteryLevel: Int
