d77786f2c9180dcf0d34fdbde43d6c30da52783d
Thierry Pelais de Oliveira <compasso-thierry.oliveira@ailos.coop.br>
2022-07-06 00:56:51 -0300

    feat: add android platform specific method to get battery level

--- android/app/src/main/kotlin/com/example/flutter_platform_specific_examples/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_platform_specific_examples/MainActivity.kt
@@ -1,6 +1,27 @@
 package com.example.flutter_platform_specific_examples
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        }
+        return batteryLevel
+    }
 }
