430411591b4caf7ad38b3a295eb5b050ae8cfc8d
Thierry Pelais de Oliveira <compasso-thierry.oliveira@ailos.coop.br>
2022-07-06 01:22:53 -0300

    feat: change MaterialApp home to HomePage

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../battery_level/pages/battery_level_page.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -10,12 +12,15 @@ class HomePage extends StatelessWidget {
           title: const Text('Platform Specific Examples'),
           centerTitle: true,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {}, child: const Text('Battery level')),
-          ],
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const BatteryLevelPage()));
+              },
+              child: const Text('Battery level')),
         ),
       );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_specific_examples/features/home/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: HomePage(),
     );
   }
 }
