99b32e4a3f541d8ccc68b67d66b5ec26c2ce870a
Thierry Pelais de Oliveira <compasso-thierry.oliveira@ailos.coop.br>
2022-07-06 01:12:03 -0300

    feat: add ios platform specific method to get battery level

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,20 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+     
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+private func receiveBatteryLevel(result: FlutterResult) {
+  let device = UIDevice.current
+  device.isBatteryMonitoringEnabled = true
+  if device.batteryState == UIDevice.BatteryState.unknown {
+    result(FlutterError(code: "UNAVAILABLE",
+                        message: "Battery level not available.",
+                        details: nil))
+  } else {
+    result(Int(device.batteryLevel * 100))
   }
 }
